63bd7a1187ed9288a5aa84a1b630dc3e35fd5f30
Dave Russell <dave@daverussell.co.uk>
2022-10-16 15:41:10 +0100

    set up drawing with WebGL

--- public_html/src/WebGL.js
+++ public_html/src/WebGL.js
@@ -4,8 +4,14 @@ var gGL = null;
 function initializeGL() {
     var canvas = document.getElementById("GLCanvas");
     var gGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    
     if (gGL != null) {
         gGL.clearColor(0.0, 0.8, 0.0, 1.0) // set colour to be cleared
+    
+        // initialise vertex buffers
+        initSquareBuffer(); // Defined in VertexBuffer.js
+        // Now load and compile the vertex and fragment shaders
+        initSimpleShader("VertexShader", "FragmentShader"); // Defined in the index.html
     } else {
         document.write("<br><b>WebGL is not supported</b>");
     }
