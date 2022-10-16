09f98e0a63f36c99dac30416f5e534fff2b69acc
Dave Russell <dave@daverussell.co.uk>
2022-10-16 15:05:01 +0100

    create seperate javascript file

--- public_html/src/WebGL.js
+++ public_html/src/WebGL.js
@@ -1,2 +1,21 @@
 "use strict"
 var gGL = null;
+
+function initializeGL() {
+    var canvas = document.getElementById("GLCanvas");
+    var gGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    if (gGL != null) {
+        gGL.clearColor(0.0, 0.8, 0.0, 1.0) // set colour to be cleared
+    } else {
+        document.write("<br><b>WebGL is not supported</b>");
+    }
+}
+
+function clearCanvas() {
+    gGL.clear(gl.COLOR_BUFFER_BIT); // clear colours
+}
+
+function doGLDraw() {
+    initializeGL();
+    clearCanvas();
+}
