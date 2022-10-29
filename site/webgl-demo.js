798f8b1bfacc2806d06b5b5a69c04182e8e4bb1f
Dave Russell <dave@daverussell.co.uk>
2022-10-29 05:04:24 +0100

    loading texture

--- site/webgl-demo.js
+++ site/webgl-demo.js
@@ -66,6 +66,17 @@ function main() {
   // objects we'll be drawing.
   const buffers = initBuffers(gl);
 
+  const texture = loadTexture(gl, "cubetexture.png");
+
+  // Browsers copy pixels from the loaded image in top-to-bottom order —
+  // from the top-left corner; but WebGL wants the pixels in bottom-to-top
+  // order — starting from the bottom-left corner. So in order to prevent
+  // the resulting image texture from having the wrong orientation when
+  // rendered, we need to make the following call, to cause the pixels to
+  // be flipped into the bottom-to-top order that WebGL expects.
+  // See jameshfisher.com/2020/10/22/why-is-my-webgl-texture-upside-down
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  
   let then = 0;
 
   // Draw the scene repeatedly
@@ -199,6 +210,60 @@ function initBuffers(gl) {
     color: colorBuffer,
     indices: indexBuffer,
   };
+}
+
+//
+// Initialize a texture and load an image.
+// When the image finished loading copy it into the texture.
+//
+function loadTexture(gl, url) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Because images have to be downloaded over the internet
+  // they might take a moment until they are ready.
+  // Until then put a single pixel in the texture so we can
+  // use it immediately. When the image has finished downloading
+  // we'll update the texture with the contents of the image.
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const width = 1;
+  const height = 1;
+  const border = 0;
+  const srcFormat = gl.RGBA;
+  const srcType = gl.UNSIGNED_BYTE;
+  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                pixel);
+
+  const image = new Image();
+  image.onload = () => {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  srcFormat, srcType, image);
+
+    // WebGL1 has different requirements for power of 2 images
+    // vs non power of 2 images so check if the image is a
+    // power of 2 in both dimensions.
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       // Yes, it's a power of 2. Generate mips.
+       gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       // No, it's not a power of 2. Turn off mips and set
+       // wrapping to clamp to edge
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+  };
+  image.src = url;
+
+  return texture;
+}
+
+function isPowerOf2(value) {
+  return (value & (value - 1)) === 0;
 }
 
 
